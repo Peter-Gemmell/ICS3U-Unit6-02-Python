d41b623a52ec20a6d9ffd80621ac1e6f95c1e38a
peter.gemmell <peter.gemmell@mths.ca>
2022-05-19 18:09:04 +0000

    Theatre

---
+++ largest_number.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Created by Peter Gemmell
+# Created on may 2022
+# This program program creates 10 random numbers & finds largest number
+
+import random
+
+
+def find_largest(array=[]):
+    largest_number = 0
+    loop_counter = 0
+    while loop_counter < len(array):
+        if array[loop_counter] > largest_number:
+            largest_number = array[loop_counter]
+        loop_counter += 1
+    return largest_number
+
+
+def main():
+    # I calculate circumference
+
+    # process
+    list_numbers = []
+    for loop_counter in range(0, 10):
+        random_number = random.randint(1, 100)
+        list_numbers.append(random_number)
+        print("The random number is : {0}".format(random_number))
+
+    # call functions
+    largest_number = find_largest(list_numbers)
+
+    # output
+    print("\nThe largest number is {}.".format(largest_number))
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
